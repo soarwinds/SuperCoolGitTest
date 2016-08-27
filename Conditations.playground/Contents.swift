40a5b7427558c118ede83e0d9de4f63cd84909fc
Chao Yu <chao@chao-mbp2015.local>
2016-08-27 21:34:27 +1000

    playground conditions

---
+++ Conditations.playground/Contents.swift
@@ -0,0 +1,53 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+var isMyHouseOnFire: Bool = false
+
+var anotherBool = true
+
+if isMyHouseOnFire {
+    print("Someone get me some water")
+} else {
+    print("Someone get some fire for my house")
+}
+
+var result = true == true
+result = true == false
+result = false == false
+
+var accountTotal = 300.33
+var newCallOfDutyGame = 59.98
+
+if accountTotal >= newCallOfDutyGame {
+    print("I just purchased the game")
+} else {
+    print("I dont have enough money")
+}
+
+var name = "Jackie"
+
+if name == "Jackie" {
+    print("Your name is Jack")
+} else {
+    print("Your name is not Jack")
+}
+
+var numberA = 100
+var numberB = 120
+var numberC = 567
+var numberD = 792
+
+var finalNumber = 567
+
+if numberA == finalNumber {
+    print(numberA)
+} else if numberA == finalNumber {
+    print(numberB)
+} else if numberC == finalNumber {
+    print(numberC)
+} else {
+    print("None of the values were equal to finalNumbe4r")
+}
+
