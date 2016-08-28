451bb933b1b8c9cf7b7c260e028d397af6302a23
Chao Yu <chao@chao-mbp2015.local>
2016-08-28 19:48:48 +1000

    Exercise - Functions

---
+++ functions-exercise.playground/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func add(numberA: Double, numberB: Double) -> Double {
+    return (numberA + numberB)
+}
+
+func subtract(numberA: Int, numberB: Int) -> Int {
+    return numberA - numberB
+}
+
+func multiply(numberA: Float, numberB: Float) -> Float {
+    return numberA * numberB
+}
+
+func divide(numberA: Double, numberB: Double) -> Double {
+    return numberA / numberB
+}
+
+add(1.1, numberB: 2.2)
+
+subtract(1, numberB: 2)
+
+multiply(0.1, numberB: 798.6)
+
+divide(1.2, numberB: 0.2) // why 1.2/0.2 = 5.999999....
+
+
