57bf45c0c4324f7ed02f79669c657360e51af9fb
Chao Yu <chao@chao-mbp2015.local>
2016-08-28 11:54:20 +1000

    Conditionals-2

--- Conditionals-2.playground/Contents.swift
+++ Conditionals-2.playground/Contents.swift
@@ -2,4 +2,27 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var myAccount = 1000.00
+var myFriendsAccouunt = 2000.00
+var myFriendsFriendsAccount = 3000.00
+
+if myAccount > 900 && myFriendsAccouunt > 1500 && myFriendsFriendsAccount > 2000.00 {
+    print("We've got mocho moola!")
+}
+
+var playerAAlive = true
+var playerBAlive = false
+var playerCAlive = true
+
+if playerAAlive == false || playerBAlive == false || playerCAlive == false {
+    print("One of your team members is down!")
+}
+
+// Can I retire???
+var age = 23
+var account = 50
+var richUncleThatDiedWhoLeftMeAnInheritance = true
+
+if account > 70000 && age >= 60 || richUncleThatDiedWhoLeftMeAnInheritance == true {
+    print("You can retire!!!")
+}
